f2b7cbf5193a9f97603e5d0a0f9ae4ae7507e71d
Marcelo A. de L. Santos <contato.maxhero@gmail.com>
2019-04-15 05:32:39 -0300

    add dependencies

---
+++ src/components/App/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux"
+import './App.css';
+
+function App({}){
+  return (
+      <div className="App">
+        
+      </div>
+    );
+}
+
+function mapToStateProps(){
+  return {};
+}
+
+function mapDispatchToProps(){
+  return {};
+}
+
+export default connect(mapToStateProps, mapDispatchToProps)(App);
